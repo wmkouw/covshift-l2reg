6ab9ea68d336b637e03dcdd2c61d98fb76ee3dfc
Wouter M. Kouw <wmkouw@gmail.com>
2017-04-25 15:21:54 +0200

    Synthetic - figure generation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CV-XVAL: Covariate shift - crossvalidation
+# Covariate shift - crossvalidation
 
 This repository contains experiments and MATLAB code accompanying the paper:
 
@@ -12,10 +12,4 @@ The folder _experiment-*_ contains a file that runs an experiment on a dataset, 
 
 ## Dependencies:
 
-- Mark Schmidt's optimization toolbox minFunc: https://www.cs.ubc.ca/~schmidtm/Software/minFunc.html
 - Dmitry Savransky's rejection sampler: https://nl.mathworks.com/matlabcentral/fileexchange/27590-simple-rejection-sampling?requestedDomain=www.mathworks.com
-
-
-
-
-
--- experiment-synth/exp_cvxval_synth.m
+++ experiment-synth/exp_cvxval_synth.m
@@ -1,132 +1,145 @@
-% Script to run some covariate shift experiments
-close all; clear all;
-addpath(genpath('..\util'))
+function [MSE,minLambda,Lambda,S,W,params] = exp_cvxval_synth(varargin)
+% Experiment with regularization parameter estimation on synthetic data
+%
+% Wouter Kouw
+% Last update: 2017-04-24
 
-% True distribution Parameters
-N = 1e2;
-M = 1e2;
-theta_Xy0 = [-1 1];
-theta_Xy1 = [ 1 1];
-py0 = 1./2;
-py1 = 1./2;
-savnm = 'results\varshift';
+% Add utility functions to path
+addpath(genpath('../util'));
+if isempty(which('sampleDist')); error('Please add sampleDist to the addpath'); end
 
-% Importance weight estimator
-iwT = 'gauss';
+% Parse
+p = inputParser;
+addOptional(p, 'S', [1 2 3 4].^2);
+addOptional(p, 'Lambda', linspace(-100,500,101));
+addOptional(p, 'N', 100);
+addOptional(p, 'M', 100);
+addOptional(p, 'nR', 10);
+addOptional(p, 'domainLimits', [-20 20]);
+addOptional(p, 'save', false);
+addOptional(p, 'saveName', 'results/results_exp_cvxval_synth');
+addOptional(p, 'iwe', 'true');
+parse(p, varargin{:});
+
+% Source domain
+params.X_yn = [-1 1];
+params.X_yp = [ 1 1];
+pX_yn = @(x) normpdf(x, params.X_yn(1), sqrt(params.X_yn(2)));
+pX_yp = @(x) normpdf(x, params.X_yp(1), sqrt(params.X_yp(2)));
+pyn = 1./2;
+pyp = 1./2;
+pX = @(x) pX_yn(x)*pyn + pX_yp(x)*pyp;
 
 % Lambda range
-Lambda = linspace(-200,500,101);
-nLl = length(Lambda);
+Lambda = p.Results.Lambda;
+nLa = length(Lambda);
 
-% Variances
-P = [0.1 0.5 1 2 3 4];
-nP = length(P);
+% Target variance range
+S = p.Results.S;
+nS = length(S);
 
-% Number of repeats
-nR = 100;
+% Set domain limits
+d = linspace(p.Results.domainLimits(1),p.Results.domainLimits(2), 201);
 
-% Optimizer
-options.Method = 'lbfgs';
-options.Display = 'full';
-options.maxIter = 1e4;
-options.xTol = 1e-7;
-options.DerivativeCheck = 'off';
+% Preallocate variables
+W = cell(nS,p.Results.nR);
+MSE.V = zeros(nS,nLa,p.Results.nR);
+MSE.W = zeros(nS,nLa,p.Results.nR);
+MSE.Z = zeros(nS,nLa,p.Results.nR);
+minl.V = zeros(nS,p.Results.nR);
+minl.W = zeros(nS,p.Results.nR);
+minl.Z = zeros(nS,p.Results.nR);
 
-W = cell(nP,nR);
-MSE_X = zeros(nP,nLl,nR);
-dMSE_X = zeros(nP,nLl,nR);
-MSE_W = zeros(nP,nLl,nR);
-dMSE_W = zeros(nP,nLl,nR);
-MSE_Z = zeros(nP,nLl,nR);
-dMSE_Z = zeros(nP,nLl,nR);
-minl_X = zeros(nP,nR);
-minf_X = zeros(nP,nR);
-minl_W = zeros(nP,nR);
-minf_W = zeros(nP,nR);
-minl_Z = zeros(nP,nR);
-minf_Z = zeros(nP,nR);
-
-xl = [-5 5];
-zl = [-5 5];
-x = linspace( xl(1),xl(2),101);
-z = linspace(-xl(2),xl(2),101);
-
-for p = 1:nP
-    for r = 1:nR
+for r = 1:p.Results.nR
+    % Report progress
+	fprintf('Running repeat %d / %d \n', r, p.Results.nR);
         
-        % Target variance parameters
-        theta_Zy0 = [-1 P(p)];
-        theta_Zy1 = [ 1 P(p)];
-        pZ_y0 = @(z) pdf('Normal', z, theta_Zy0(1), theta_Zy0(2));
-        pZ_y1 = @(z) pdf('Normal', z, theta_Zy1(1), theta_Zy1(2));
-        pZ = @(z) (pZ_y0(z).*py0 + pZ_y1(z).*py1);
+    for s = 1:nS
+        
+        % Target variance parameterse
+        params.Z_yn = [-1 S(s)];
+        params.Z_yp = [ 1 S(s)];
+        pZ_yn = @(z) normpdf(z, params.Z_yn(1), sqrt(params.Z_yn(2)));
+        pZ_yp = @(z) normpdf(z, params.Z_yp(1), sqrt(params.Z_yp(2)));
+        pZ = @(z) (pZ_yn(z).*pyn + pZ_yp(z).*pyp);
         
         % Generate class-conditional distributions and sample sets
-        [Xy_p,Xy_n,Zy_p,Zy_n,pXy_p,pXy_n,pZy_p,pZy_n] = gen_cvshift('theta_Xy0', theta_Xy0,'theta_Xy1', theta_Xy1, 'N', N, 'M', M, 'pZ', pZ);
+        [Xy_p,Xy_n,Zy_p,Zy_n] = gen_cvshift('theta_Xy0', params.X_yn,'theta_Xy1', ...
+            params.X_yp, 'N', p.Results.N, 'M', p.Results.M, 'pZ', pZ, ...
+            'ub', 1./sqrt(2*pi*S(s)), 'xl', p.Results.domainLimits, ...
+            'zl', p.Results.domainLimits);
         
-        Vy_n = sampleDist(pXy_n,1.1./sqrt(2*pi),round(N.*py0),[zl(1) zl(2)], false);
-        Vy_p = sampleDist(pXy_p,1.1./sqrt(2*pi),round(N.*py1),[zl(1) zl(2)], false);
+        % Sample training data
+        Vy_n = sampleDist(pX_yn, 1./sqrt(2*pi), round(p.Results.N.*pyn), p.Results.domainLimits, false);
+        Vy_p = sampleDist(pX_yp, 1./sqrt(2*pi), round(p.Results.N.*pyp), p.Results.domainLimits, false);
         
-        % Concatenate to dataset
-        X = [Xy_n; Xy_p];
+        % Concatenate to source validation set 
         V = [Vy_n; Vy_p];
-        Z = [Zy_n; Zy_p];
-        yX = [-ones(size(Xy_n,1),1); ones(size(Xy_p,1),1)];
         yV = [-ones(size(Vy_n,1),1); ones(size(Vy_p,1),1)];
-        yZ = [-ones(size(Xy_n,1),1); ones(size(Xy_p,1),1)];
+        
+        % Concatenate to target validation set
+        Z = [Zy_n; Zy_p];
+        yZ = [-ones(size(Zy_n,1),1); ones(size(Zy_p,1),1)];
+        
+        % Concatenate to source training set
+        X = [Xy_n; Xy_p];
+        yX = [-ones(size(Xy_n,1),1); ones(size(Xy_p,1),1)];
         
         % Obtain importance weights
-        switch lower(iwT)
+        switch lower(p.Results.iwe)
             case 'none'
-                W{p,r} = ones(1,size(V,1));
+                W{s,r} = ones(1,size(V,1));
             case 'true'
-                A = zeros(1,size(V,1));
-                A(yV==-1) = pZy_n(V(yV==-1))./pXy_n(V(yV==-1));
-                A(yV==1) = pZy_p(V(yV==1))./pXy_p(V(yV==1));
-                W{p,r} = A;
-            case 'cauchy'
-                W{p,r} = iw_Cauchy(V,Z,0,realmax);
+                W{s,r} = pZ(V)./pX(V);
             case 'gauss'
-                W{p,r} = iw_Gauss(V',Z',0,realmax);
+                W{s,r} = iwe_Gauss(V',Z');
             case 'kmm'
-                W{p,r} = iw_KMM(V',Z',0,realmax);
+                W{s,r} = iwe_KMM(V',Z',0,realmax);
             case 'kliep'
-                W{p,r} = iw_KLIEP(V,Z,0,realmax);
+                W{s,r} = iwe_KLIEP(V,Z,0,realmax);
             case 'nnew'
-                W{p,r} = iw_NNeW(V,Z,0,realmax, 'Laplace', 1);
+                W{s,r} = iwe_NNeW(V,Z,0,realmax, 'Laplace', 1);
             otherwise
                 error(['Unknown importance weight estimator']);
         end
-        W{p,r} = diag(W{p,r});
         
         % Augment data
         Xa = [X ones(size(X,1),1)];
         Va = [V ones(size(V,1),1)];
         Za = [Z ones(size(Z,1),1)];
         
-        % Obtain lambda-curve
-        for l = 1:nLl
-            [MSE_X(p,l,r),dMSE_X(p,l,r)] = cv_lambda_grad2(Lambda(l),Xa,Va,yX,yV);
-            [MSE_W(p,l,r),dMSE_W(p,l,r)] = cv_lambda_grad2(Lambda(l),Xa,Va,yX,yV,W{p,r});
-            [MSE_Z(p,l,r),dMSE_Z(p,l,r)] = cv_lambda_grad2(Lambda(l),Xa,Za,yX,yZ);
+        % Loop over regularization parameter values
+        for l = 1:nLa
+            
+            % Analytical solution to regularized least-squares classifier
+            theta = (Xa'*Xa + Lambda(l)*eye(2))\Xa'*yX;
+            
+            % Mean squared error curves
+            MSE.V(s,l,r) = mean((Va*theta - yV).^2,1);
+            MSE.W(s,l,r) = mean((Va*theta - yV).^2.*W{s,r},1);
+            MSE.Z(s,l,r) = mean((Za*theta - yZ).^2,1);
         end
-
-        % Find minima
-        [minf_X(p,r),minl_X(p,r)] = min(MSE_X(p,:,r));
-        [minf_W(p,r),minl_W(p,r)] = min(MSE_W(p,:,r));
-        [minf_Z(p,r),minl_Z(p,r)] = min(MSE_Z(p,:,r));
+        
+        % Minima of mean squared error curves
+        [~,minl.V(s,r)] = min(MSE.V(s,:,r));
+        [~,minl.W(s,r)] = min(MSE.W(s,:,r));
+        [~,minl.Z(s,r)] = min(MSE.Z(s,:,r));
     end
 end
 
-minLa_X = Lambda(minl_X);
-minLa_W = Lambda(minl_W);
-minLa_Z = Lambda(minl_Z);
+% Arg min of mean squared error curves
+minLambda.V = Lambda(minl.V);
+minLambda.W = Lambda(minl.W);
+minLambda.Z = Lambda(minl.Z);
 
-di = 1; while exist(['results_iw-' iwT '_' savnm '_' num2str(di) '.mat'], 'file'); di = di+1; end
-fn = [savnm '_iw-' iwT '_' num2str(di) '.mat'];
-disp(['Done. Writing to ' fn]);
-save(fn, 'minLa_X', 'minLa_W', 'minLa_Z', 'minf_X', 'minl_X', 'minf_W', 'minl_W', 'minf_Z', 'minl_Z');
+% Write to file
+if p.Results.save
+    fn = [p.Results.saveName '.mat'];
+    disp(['Done. Writing to ' fn]);
+    save(fn, 'MSE', 'minLambda', 'Lambda', 'theta', 'S', 'W');
+end
 
+end
 
 
 
